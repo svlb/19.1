b6cdc637a6e203054085f0db5a70698d9c02f433
svlb <143376603+svlb@users.noreply.github.com>
2023-12-01 17:52:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = 'localhost'
+serverPort = 8080
+filename = 'index.html'
+
+class MyServer (BaseHTTPRequestHandler ):
+
+    def __get_content (self):
+        with open (filename, 'r', encoding="utf-8") as file:
+            result = file.read()
+            return result
+
+    def do_GET (self):
+        query_components = parse_qs (urlparse(self.path).query)
+        page_content = self.__get_content()
+        self.send_response(200)
+        self.send_header ("Content-type","text/html")
+        self.end_headers()
+        self.wfile.write(bytes (page_content, "utf-8"))
 
 
-# Hастройки запуска
-hostname = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
-
-
-class MyServer(BaseHTTPRequestHandler):
-
-    """ Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
-    """
-
-    def do_get(self):
-
-        """ Метод для обработки входящих GET-запросов """
-
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8")) # Тело ответа"))
-
-
-if __name__ == "__main__":
-
-    # Инициализация веб-сервера, который будет по заданным параметрам в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
-    webServer = HTTPServer((hostname, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostname, serverPort))
+if __name__ == '__main__':
+    webServer = HTTPServer ((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
-        webServer.serve_forever()
+        webServer. serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
         pass
 
-    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
